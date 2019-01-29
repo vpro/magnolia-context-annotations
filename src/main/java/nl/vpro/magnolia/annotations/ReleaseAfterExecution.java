185c1420dc13c534f39d80b7f78b347ba753eac0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-29 11:41:14 +0100

    javadoc.

--- src/main/java/nl/vpro/magnolia/annotations/ReleaseAfterExecution.java
+++ src/main/java/nl/vpro/magnolia/annotations/ReleaseAfterExecution.java
@@ -2,10 +2,19 @@ package nl.vpro.magnolia.annotations;
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 1.2
  */
 public enum ReleaseAfterExecution {
+    /**
+     * The system context will be released in any case.
+     */
     TRUE,
+    /**
+     * The system context will be released in no case. Since you cannot always know if the system context will be used in the same thread again, this may be the best value.
+     */
     FALSE,
+     /**
+     * The system context will be released if there is no current system context to start with.
+     */
     SMART;
 }
